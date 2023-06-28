3bfdca69284fbe8d850f5540802b7ff070a67c57
Chan Hui Ting <chanht2701@gmail.com>
2023-06-28 11:26:41 +0800

    Added code for step 4 & 5

--- app/src/main/java/sg/edu/np/mad_wk2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad_wk2/MainActivity.java
@@ -8,14 +8,30 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
+    private Button followButton;
     final String TITLE = "Main Activity";
+    private User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
         Log.v(TITLE, "On Create!");
+
+        user = new User("username", "description", 1, true);
+        followButton = findViewById(R.id.button);
+
+        userChangeButton();
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.followed = !user.followed;
+                userChangeButton();
+            }
+        });
+
     }
 
     @Override
@@ -48,5 +64,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
         Log.v(TITLE,"On Destroy!");
+    }
+
+    public void userChangeButton(){
+        if (user.followed){
+            followButton.setText("Unfollow");
+        }
+        else{
+            followButton.setText("Follow");
+        }
     }
 }
